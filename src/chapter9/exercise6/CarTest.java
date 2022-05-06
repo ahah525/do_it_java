00953d04dcb28d8fde2fe4caea83e2fc2062a6c0
ahah525 <ahah525@naver.com>
2022-05-06 14:25:59 +0900

    Do_it_JavaProgramming chapter 9 연습문제6

---
+++ src/chapter9/exercise6/CarTest.java
@@ -0,0 +1,20 @@
+package chapter9.exercise6;
+
+import java.util.ArrayList;
+
+public class CarTest {
+
+    public static void main(String[] args) {
+        ArrayList<Car> carList = new ArrayList<>();
+        carList.add(new Sonata());
+        carList.add(new Grandeur());
+        carList.add(new Avante());
+        carList.add(new Genesis());
+
+        for(Car car: carList) {
+            car.run();
+            System.out.println("=======================");
+        }
+
+    }
+}
