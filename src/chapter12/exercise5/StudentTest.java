aa3c925336c3ecaec8a4e3305e67fae16839bf6f
ahah525 <ahah525@naver.com>
2022-05-09 23:20:36 +0900

    Do_it_JavaProgramming chapter 12 연습문제5

--- src/chapter12/exercise5/StudentTest.java
+++ src/chapter12/exercise5/StudentTest.java
@@ -6,7 +6,7 @@ public class StudentTest {
     public static void main(String[] args) {
         HashSet<Student> set = new HashSet<>();
 
-        set.add(new Student(100, "홍길동"));
+        set.add(new Student(100,"홍길동"));
         set.add(new Student(200, "강감찬"));
         set.add(new Student(300, "이순신"));
         set.add(new Student(400, "정약용"));
